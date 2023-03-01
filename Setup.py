debe5bb9317d0e1cd1841c6ac93cccaa4f94b4c5
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-03-01 21:44:12 +0800

    Add: use class for block instead of a flag

--- Setup.py
+++ Setup.py
@@ -1,10 +1,42 @@
 #! /usr/bin/python3
 
 #%% import modules
-import sys
 import subprocess
 import re
 import json
+from sys import argv
+from typing import Optional
+
+#%% define the regular expression pattern
+condition_block_start_pattern:re.Pattern = re.compile(r'^#>>> ({.*})\s*')
+condition_block_end_pattern:re.Pattern = re.compile(r'^#<<<\s*')
+command_block_start_pattern:re.Pattern = re.compile(r'^#%%(.*) ({.*})\s*(#%%)?\s*')
+command_block_end_pattern:re.Pattern = re.compile(r'^#%%\s*')
+
+#%% define command block
+class CommandBlock():
+    wait_all_flag:bool = False
+
+    def __init__(self,wait_flag=False,run_flag=True) -> None:
+        self.command_to_run:str = ""
+        self.wait_flag:bool = wait_flag
+        self.run_flag:bool = run_flag
+
+    def __repr__(self) -> str:
+        return self.command_to_run
+
+    def add(self,new_command:str) -> None:
+        self.command_to_run += new_command
+
+    def run(self,data_dict:dict) -> None:
+        if self.run_flag:
+            formatted_command:str = self.command_to_run.format(**data_dict)
+            if self.wait_all_flag or self.wait_flag:
+                print("\n\n"+">"*50+"\nCommand to run:")
+                print(formatted_command,end='')
+                print("<"*50)
+                input("(Enter to run) ")          
+            subprocess.run(formatted_command,shell=True)
 
 #%% handle how to interact with user
 def user_input(cond_key,cond_value):
@@ -34,7 +66,6 @@ def user_input(cond_key,cond_value):
     else:
         raise ValueError("Unsupported type of condition value")
 
-
 #%% handle how to check an load condition 
 def check_and_load_condition(condition_dict,data_dict):
     for cond_key,cond_value in condition_dict.items():
@@ -49,72 +80,60 @@ def check_and_load_condition(condition_dict,data_dict):
             return False
     return True
 
-
-#%% define the regular expression pattern
-condition_block_start_pattern = re.compile(r'^#>>> ({.*})\s*')
-condition_block_end_pattern = re.compile(r'^#<<<\s*')
-command_block_start_pattern = re.compile(r'^#%%(.*) ({.*})\s*(#%%)?\s*')
-command_block_end_pattern = re.compile(r'^#%%\s*')
-
-
 #%% load file and execute it
 def exec_file(filepath:str,data_dict=dict()):
+    not_store_list = []
     with open(filepath,'r') as fh:
         print("Start setup...")
-        not_store_list = []
-        in_condition_block:bool = False
-        run_condition_block:bool = True
-        in_command_block:bool = False
-        run_command_block:bool = True
-        command_to_run:str = ""
+        condition_block_stack = []
+        command_block:Optional[CommandBlock] = None
         for i,line in enumerate(fh.readlines()):
             #print(line,end='')
             if match_condition_start := condition_block_start_pattern.fullmatch(line):
-                assert in_condition_block == False
-                in_condition_block = True
                 condition_dict:dict = json.loads(match_condition_start.group(1).encode('utf-8'))
-                run_condition_block = check_and_load_condition(condition_dict,data_dict)
+                condition_block_stack.append(check_and_load_condition(condition_dict,data_dict))
             elif condition_block_end_pattern.fullmatch(line):
-                assert in_condition_block == True
-                in_condition_block = False
-            elif not in_condition_block or run_condition_block:
+                condition_block_stack.pop()
+            elif not condition_block_stack or all(condition_block_stack):
                 if match_command_start := command_block_start_pattern.fullmatch(line):
-                    assert in_command_block == False
-                    in_command_block = True
-                    condition_dict:dict = json.loads(match_command_start.group(2).encode('utf-8'))
-                    run_command_block = check_and_load_condition(condition_dict,data_dict)
-                    if post_flag := match_command_start.group(1):
-                        if '*' in post_flag and not run_command_block:
+                    _match = match_command_start.groups()
+                    post_parameters:str = _match[0]
+                    condition_dict:dict = json.loads(_match[1].encode('utf-8'))
+                    is_one_line_block:bool = bool(_match[2])
+
+                    command_block = CommandBlock()
+                    command_block.run_flag = check_and_load_condition(condition_dict,data_dict)
+                    
+                    if not command_block.run_flag:
+                        if '*' in post_parameters:
                             raise ValueError(f"Unsupported condition:\n{data_dict}")
-                        if '%' in post_flag:
-                            not_store_list.extend(condition_dict.keys())
-                    elif not run_command_block:
-                        print(f"Don't match the block condition:\n{condition_dict}\nIgnore this block.")
-                    if match_command_start.group(3) == "#%%":
-                        in_command_block = False
+                        else:
+                            print(f"Doesn't match the block condition:\n{condition_dict}\nIgnore this block.")
+                    if '%' in post_parameters:
+                        not_store_list.extend(condition_dict.keys())
+                    if '@' in post_parameters:
+                        command_block.wait_flag = True
+                    if is_one_line_block:
+                        command_block = None
                 elif command_block_end_pattern.fullmatch(line):
-                    assert in_command_block == True
-                    in_command_block = False
-                    if run_command_block:
-                        #print(command_to_run)
-                        subprocess.run(command_to_run,shell=True)
+                    if command_block:
+                        command_block.run(data_dict)
+                        command_block = None
                     else:
-                        assert command_to_run == ""
-                    command_to_run = ""
-                elif in_command_block and run_command_block:
-                    command_to_run += line.format(**data_dict)
-        assert not in_condition_block and not in_condition_block
+                        raise ValueError(f"Reach end of command block, but no block exist")
+                elif command_block:
+                    command_block.add(line)
+        assert not condition_block_stack and not command_block
         print("Reach end of file, setup completely")
     store_dict = data_dict.copy()
     for key in not_store_list:
         del store_dict[key]
     return store_dict
 
-
 #%% main function
 if __name__ == '__main__':
     file_path:str = ""
-    argv_num:int = len(sys.argv)
+    argv_num:int = len(argv)
     if argv_num <= 1:
         print("No file provide, use default file path")
         file_path = "data/testfile"
@@ -122,7 +141,7 @@ if __name__ == '__main__':
         print("Too many file provide")
         exit(0)
     else :
-        file_path = sys.argv[1]
+        file_path = argv[1]
     
     #Load dictionary
     dict_path:str = "data/data_dict.json"
@@ -133,10 +152,14 @@ if __name__ == '__main__':
     except (FileNotFoundError, json.decoder.JSONDecodeError):
         pass
     
+    #user checking
+    if store_dict:
+        print(f"Load store dictionary:\n{store_dict}")
+        input("I have checked (enter to continue) ")
+
     #Execute file
     store_dict = exec_file(file_path,store_dict)
 
     #Write dictionary into file
     with open(dict_path,"w") as fh:
         json.dump(store_dict,fh,indent=2)
-
